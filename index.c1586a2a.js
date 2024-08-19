a9ffac7f74fbe1a56cde9dc6b4446878225044c8
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-19 22:12:48 +0300

    make build

---
+++ index.c1586a2a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");e.src=r.href});
+//# sourceMappingURL=index.c1586a2a.js.map
